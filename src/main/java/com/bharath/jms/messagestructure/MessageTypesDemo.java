b36111f3ec23b3639055f1f001bf769d4d047ac6
LT <ltheodor@hotmail.de>
2020-06-01 15:26:19 +0200

    38. Create Object Message

--- src/main/java/com/bharath/jms/messagestructure/MessageTypesDemo.java
+++ src/main/java/com/bharath/jms/messagestructure/MessageTypesDemo.java
@@ -1,11 +1,14 @@
 package com.bharath.jms.messagestructure;
 
+import java.io.Serializable;
+
 import javax.jms.BytesMessage;
 import javax.jms.JMSContext;
 import javax.jms.JMSException;
 import javax.jms.JMSProducer;
 import javax.jms.MapMessage;
 import javax.jms.Message;
+import javax.jms.ObjectMessage;
 import javax.jms.Queue;
 import javax.jms.StreamMessage;
 import javax.jms.TextMessage;
@@ -14,8 +17,10 @@ import javax.naming.NamingException;
 
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 
-public class MessageTypesDemo {
+public class MessageTypesDemo implements Serializable {
 	
+	  private static final long serialVersionUID = 1L;
+
 		public static void main(String[] args) throws NamingException, InterruptedException, JMSException {
 		
 		InitialContext context = new InitialContext();
@@ -38,20 +43,28 @@ public class MessageTypesDemo {
 			MapMessage mapMessage = jmsContext.createMapMessage();
 			mapMessage.setBoolean("isCreditAvailable", true);
 			
-			producer.send(queue, mapMessage);
+			ObjectMessage objectMessage = jmsContext.createObjectMessage();
+			Patient patient = new Patient();
+			patient.setId(123);
+			patient.setname("John");
+			objectMessage.setObject(patient);
+			
+//			producer.send(queue, mapMessage);
+			producer.send(queue, objectMessage);
 
 			
 //			BytesMessage messageReceived = (BytesMessage) jmsContext.createConsumer(queue).receive(5000);
 //			System.out.println(messageReceived.readUTF());
 //			System.out.println(messageReceived.readLong());
 			
-			MapMessage messageReceived = (MapMessage) jmsContext.createConsumer(queue).receive(5000);
-			System.out.println(messageReceived.getBoolean("isCreditAvailable"));
+//			MapMessage messageReceived = (MapMessage) jmsContext.createConsumer(queue).receive(5000);
+//			System.out.println(messageReceived.getBoolean("isCreditAvailable"));
 //			System.out.println(messageReceived.readFloat());
 
-			
-			
-
+			ObjectMessage messageReceived = (ObjectMessage) jmsContext.createConsumer(queue).receive(5000);
+			Patient object = (Patient) messageReceived.getObject();
+			System.out.println(object.getId());
+			System.out.println(object.getname());
 		}
 	}
 }
