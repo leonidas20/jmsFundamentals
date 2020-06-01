6f50ae34c9fa0b291183eda4e21e4a887cafffa5
LT <ltheodor@hotmail.de>
2020-06-01 12:38:10 +0200

    37. Message types in action

--- src/main/java/com/bharath/jms/messagestructure/MessageDelayDemo.java
+++ src/main/java/com/bharath/jms/messagestructure/MessageDelayDemo.java
@@ -1,39 +1,33 @@
 package com.bharath.jms.messagestructure;
 
 import javax.jms.JMSContext;
-import javax.jms.JMSException;
 import javax.jms.JMSProducer;
 import javax.jms.Message;
 import javax.jms.Queue;
-import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 
 public class MessageDelayDemo {
-	
-	
+
+
 	private static Object lookup;
 
-	public static void main(String[] args) throws NamingException, InterruptedException, JMSException {
-		
+	public static void main(String[] args) throws NamingException, InterruptedException {
+
 		InitialContext context = new InitialContext();
 		Queue queue = (Queue) context.lookup("queue/myQueue");
-		
+
 		try(ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory(); 
 			JMSContext jmsContext = cf.createContext()) {
-				
+
 			JMSProducer producer = jmsContext.createProducer();
-			TextMessage textMessage = jmsContext.createTextMessage("Arise Awake and stop not till the goal is reached");
-			textMessage.setBooleanProperty("loggedIn", true);
-			textMessage.setStringProperty("userToken", "abc123");
-			producer.send(queue, textMessage);
-			
+			producer.setDeliveryDelay(3000);
+			producer.send(queue, "Arise Awake and stop not till the goal is reached");
+
 			Message messageReceived = jmsContext.createConsumer(queue).receive(5000);
 			System.out.println(messageReceived);
-			System.out.println(messageReceived.getBooleanProperty("loggedIn"));
-			System.out.println(messageReceived.getStringProperty("userToken"));
 		}
 	}
 }
--- src/main/java/com/bharath/jms/messagestructure/MessagePropertiesDemo.java
+++ src/main/java/com/bharath/jms/messagestructure/MessagePropertiesDemo.java
@@ -1,9 +1,11 @@
 package com.bharath.jms.messagestructure;
 
 import javax.jms.JMSContext;
+import javax.jms.JMSException;
 import javax.jms.JMSProducer;
 import javax.jms.Message;
 import javax.jms.Queue;
+import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
@@ -14,7 +16,7 @@ public class MessagePropertiesDemo {
 	
 	private static Object lookup;
 
-	public static void main(String[] args) throws NamingException, InterruptedException {
+	public static void main(String[] args) throws NamingException, InterruptedException, JMSException {
 		
 		InitialContext context = new InitialContext();
 		Queue queue = (Queue) context.lookup("queue/myQueue");
@@ -23,11 +25,15 @@ public class MessagePropertiesDemo {
 			JMSContext jmsContext = cf.createContext()) {
 				
 			JMSProducer producer = jmsContext.createProducer();
-			producer.setDeliveryDelay(3000);
-			producer.send(queue, "Arise Awake and stop not till the goal is reached");
+			TextMessage textMessage = jmsContext.createTextMessage("Arise Awake and stop not till the goal is reached");
+			textMessage.setBooleanProperty("loggedIn", true);
+			textMessage.setStringProperty("userToken", "abc123");
+			producer.send(queue, textMessage);
 			
 			Message messageReceived = jmsContext.createConsumer(queue).receive(5000);
 			System.out.println(messageReceived);
+			System.out.println(messageReceived.getBooleanProperty("loggedIn"));
+			System.out.println(messageReceived.getStringProperty("userToken"));
 		}
 	}
 }
