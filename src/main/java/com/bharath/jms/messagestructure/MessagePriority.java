395035dd023b86937e1198eaa2360a89d06183a9
LT <ltheodor@hotmail.de>
2020-06-01 08:38:16 +0200

    27. Default Priority

--- src/main/java/com/bharath/jms/messagestructure/MessagePriority.java
+++ src/main/java/com/bharath/jms/messagestructure/MessagePriority.java
@@ -2,7 +2,9 @@ package com.bharath.jms.messagestructure;
 
 import javax.jms.JMSConsumer;
 import javax.jms.JMSContext;
+import javax.jms.JMSException;
 import javax.jms.JMSProducer;
+import javax.jms.Message;
 import javax.jms.Queue;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -26,20 +28,25 @@ public class MessagePriority {
 			messages[1] = "Message Two";
 			messages[2] = "Message Three";
 			
-			producer.setPriority(3);
+//			producer.setPriority(3);
 			producer.send(queue, messages[0]);
 
-			producer.setPriority(4);
+//			producer.setPriority(4);
 			producer.send(queue, messages[1]);
 			
-			producer.setPriority(9);
+//			producer.setPriority(9);
 			producer.send(queue, messages[2]);
 			
 			JMSConsumer consumer = jmsContext.createConsumer(queue);
 			
 			for (int i=0;i<3;i++) {
-				System.out.println(consumer.receiveBody(String.class));
+				Message receivedMessage = consumer.receive();
+				System.out.println(receivedMessage.getJMSPriority());
+//				System.out.println(consumer.receiveBody(String.class));
+			
 			}
+		} catch (JMSException e) {
+			e.printStackTrace();
 		}
 	}
 }
